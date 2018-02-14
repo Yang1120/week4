3065568c63d7842e0ccdd23900c9d03588214150
Yang1120 <souyang120@hotmail.com>
2018-02-14 07:02:01 +0000

    week4.sh

---
+++ week4.sh
@@ -0,0 +1,26 @@
+#/!/bin/sh
+
+echo "This is week4 script"
+
+pwd
+
+ls -l
+
+chmod 755 week4.sh
+
+mkdir dir_1 dir_2
+
+echo "testing1"  > dir_1/file_1
+echo "testing2"  > dir_1/file_2
+echo "testing3"  > dir_1/file_3
+echo "testing4"  > dir_2/file_4
+
+In dir_1/file_3 dir_2/file_4
+
+ls -iR dir_*
+
+cat /proc/cmdline
+
+ 
+
+
